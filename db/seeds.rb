a68dd8b0372d5301e0879607eddb9584ae5e1348
Jamie Shannon <shannon.jamie@gmail.com>
2019-02-21 12:48:29 +0000

    test push

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+2.times do |topic|
+  Topic.create!(
+    title: "Topic ##{topic}"
+  )
+end
+
+puts "2 topics created."
 
 5.times do |blog|
   Blog.create!(
     title: "Blog Post No.#{blog}",
-    body: "This is a blog post."
+    body: "This is a blog post.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -24,10 +32,20 @@ end
 
 puts "5 Skills created."
 
-9.times do |portfolio|
+8.times do |portfolio|
   Portfolio.create!(
     title: "Portfolio #{portfolio}",
-    subtitle: "My great service",
+    subtitle: "Ruby on Rails",
+    body: "Some random body",
+    main_image: "https://via.placeholder.com/600x400",
+    thumb_image: "https://via.placeholder.com/350x200"
+  )
+  end
+
+1.times do |portfolio|
+  Portfolio.create!(
+    title: "Portfolio #{portfolio}",
+    subtitle: "Angular",
     body: "Some random body",
     main_image: "https://via.placeholder.com/600x400",
     thumb_image: "https://via.placeholder.com/350x200"
