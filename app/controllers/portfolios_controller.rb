703f4499995122f34502bc869d8f054617baffcf
Jamie Shannon <shannon.jamie@gmail.com>
2019-03-19 13:54:34 +0000

    Implemented form configuration for nested attributes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -12,6 +12,7 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end
 
   def create
@@ -57,6 +58,6 @@ class PortfoliosController < ApplicationController
 
   private
   def blog_params
-    params.require(:portfolio).permit(:title, :subtitle, :body)
+    params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
   end
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,12 +1,17 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
+  accepts_nested_attributes_for :technologies,
+                                reject_if: lambda { |attrs| attrs['name'].blank? }
+
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+  # Custom scope type 1. e.g. Portfolio.angular from controller returns this.
   def self.angular
     where(subtitle: 'Angular')
   end
 
+  # Custom scope type 2. e.g. Portfolio.ruby_on_rails from controller returns this.
   scope :ruby_on_rails, -> { where(susbtitle: 'Ruby on Rails') }
 
   after_initialize :set_defaults
