08fc87fca87750a6ec6d62438c1ba17f373a0919
Jamie Shannon <shannon.jamie@gmail.com>
2019-04-13 16:20:22 +0100

    Implemented basic auth with devise.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_01_101521) do
+ActiveRecord::Schema.define(version: 2019_04_13_151351) do
 
   create_table "blogs", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "title"
@@ -439,6 +439,19 @@ ActiveRecord::Schema.define(version: 2019_03_01_101521) do
     t.timestamp "password_last_changed"
     t.integer "password_lifetime", limit: 2, unsigned: true
     t.string "account_locked", limit: 1, default: "N", null: false, collation: "utf8_general_ci"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "blogs", "topics"
