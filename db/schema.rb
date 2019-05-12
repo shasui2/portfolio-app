5183d919a6809c38da27e7ffa0a5920b372ea985
Jamie Shannon <shannon.jamie@gmail.com>
2019-05-12 21:33:44 +0100

    Added installation for petergate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_13_151351) do
+ActiveRecord::Schema.define(version: 2019_05_12_203110) do
 
   create_table "blogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2019_04_13_151351) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "roles"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
